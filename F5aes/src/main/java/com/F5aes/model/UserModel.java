c9de92ce6019c3953f6d7a1bdbcf84deb04aa91e
Brianclikclak <brianclikclak@hotmail.com>
2023-09-20 09:58:34 +0200

    update table

--- F5aes/src/main/java/com/F5aes/model/UserModel.java
+++ F5aes/src/main/java/com/F5aes/model/UserModel.java
@@ -18,16 +18,14 @@ public class UserModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private  String username;
+	private String username;
 	private String email;
-	private  String password;
+	private String password;
 	private String repeatPassword;
-	@ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleModel.class,
-			cascade = CascadeType.PERSIST)
-	@JoinTable(name = "user_roles",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleModel.class, cascade = CascadeType.PERSIST)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<RoleModel> roleModels;
+
 	public void addRole(RoleModel role) {
 		roleModels.add(role);
 	}
